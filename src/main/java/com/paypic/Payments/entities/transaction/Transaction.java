cf2a5b9d3047f6c91bd6d0b4e5da47ebae3744cc
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-19 11:08:42 -0300

    Feat: Cria a classe de transacao para representar essa entidade do sistema e banco de dados.

---
+++ src/main/java/com/paypic/Payments/entities/transaction/Transaction.java
@@ -0,0 +1,28 @@
+package com.paypic.Payments.entities.transaction;
+
+import com.paypic.Payments.entities.client.User;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "tb_transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
