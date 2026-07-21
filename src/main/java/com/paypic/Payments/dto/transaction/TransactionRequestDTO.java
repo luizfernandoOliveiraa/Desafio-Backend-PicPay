726e0655ac6d4d92a26c8b7966b5f16287636965
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-21 19:15:04 -0300

    feat: adiciona validacoes @notNull, @email e @positive nos dtos

--- src/main/java/com/paypic/Payments/dto/transaction/TransactionRequestDTO.java
+++ src/main/java/com/paypic/Payments/dto/transaction/TransactionRequestDTO.java
@@ -1,22 +1,25 @@
 package com.paypic.Payments.dto.transaction;
 
-import com.paypic.Payments.entities.client.User;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionRequestDTO {
 
+    @NotNull(message = "Valor da transferência é obrigatório")
+    @Positive
     private BigDecimal amount;
+    @NotNull(message = "Id do remetente é obrigatório")
     private Long senderId;
+    @NotNull(message = "Id do recebedor é obrigatório")
     private Long receiverId;
 
 }
--- src/main/java/com/paypic/Payments/dto/user/UserRequestDTO.java
+++ src/main/java/com/paypic/Payments/dto/user/UserRequestDTO.java
@@ -2,6 +2,10 @@ package com.paypic.Payments.dto.user;
 
 import com.paypic.Payments.entities.client.TipoCliente;
 import com.paypic.Payments.entities.client.TipoPessoa;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +17,29 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class UserRequestDTO {
 
+    @NotNull(message = "First name é obrigatório")
     private String firstName;
+
+    @NotNull(message = "Last name é obrigatório")
     private String lastName;
+
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotNull(message = "Saldo é obrigatório")
+    @Positive(message = "Saldo deve ser positivo")
     private BigDecimal saldo;
+
+    @NotNull(message = "Documento identificação é obrigatório")
     private String documentoIdentificacao;
+
+    @NotNull(message = "Tipo cliente é obrigatório")
     private TipoCliente tipoCliente;
+
+    @NotNull(message = "Tipo pessoa é obrigatório")
     private TipoPessoa tipoPessoa;
+
+    @NotNull(message = "Senha é obrigatória")
     private String senha;
 
 }
